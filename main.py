da8b49a11c4c3054ef885d62f1ab208f5871a22f
Your Name <your_email@whatever.com>
2025-10-25 03:27:58 +0400

    extended and impmroved

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-field_height = 8
+field_height = get_world_size()
 
 field_map = [
 	[
@@ -6,11 +6,17 @@ field_map = [
 	]
 ]
 
+def moiser(water):
+	if water and get_water() < 0.9:
+		use_item(Items.Water)
+
 def handle_hay(i, water, fertilize):
+	moiser(water)
 	if can_harvest():
 		harvest()
 
 def handle_carrot(i, water, fertilize):
+	moiser(water)
 	gt = get_ground_type()
 	if can_harvest():
 		harvest()
@@ -19,34 +25,47 @@ def handle_carrot(i, water, fertilize):
 		plant(Entities.Carrot)
 	elif gt == Grounds.Soil:
 		plant(Entities.Carrot)
+		if fertilize:
+			use_item(Items.Fertilizer)
 		
 def handle_pumpkin(i, water, fertilize):
+	moiser(water)
 	if can_harvest():
 		harvest()
-		if get_ground_type() != Grounds.Soil:
-				till()
+
+		# if get_entity_type() == Entities.Pumpkin:
+		# 	print(measure(South))
+		# 	if measure() > 3:
+		# 		harvest()
+		# 	print("no harvest")
+		# else:
+		# 	harvest()
+		
+		
+	if get_ground_type() != Grounds.Soil:
+			till()
 
 	if get_ground_type() == Grounds.Soil:
 		plant(Entities.Pumpkin)
-		if water:
-			use_item(Items.Water)
 		if fertilize:
 			use_item(Items.Fertilizer)
 
 def handle_bush(i, water, fertilize):
+	moiser(water)
 	if can_harvest():
 		harvest()
 		plant(Entities.Bush)
 		
 def handle_trees(i, water, fertilize):
-		if can_harvest():
-			harvest()
-		if (i % 2) != 0:
-			plant(Entities.Tree)
-			if water:
-				use_item(Items.Water)
-			if fertilize:
-				use_item(Items.Fertilizer)
+	# moiser(water)
+	if can_harvest():
+		harvest()
+	if (i % 2) != 0:
+		plant(Entities.Tree)
+		if water:
+			use_item(Items.Water)
+		if fertilize:
+			use_item(Items.Fertilizer)
 
 def handle_row(action, vertical_way, horizontal_way, water, fertilize):
 	for i in range(field_height):
@@ -59,6 +78,8 @@ def handle_row(action, vertical_way, horizontal_way, water, fertilize):
 
 clear()
 
+change_hat(Hats.Wizard_Hat)
+
 while True:
 	
 #main route
@@ -70,13 +91,21 @@ while True:
 	
 	handle_row(handle_pumpkin, South, East, True, True)
 	
-	handle_row(handle_carrot, North, East, False, False)
+	handle_row(handle_carrot, North, East, True, False)
 	
-	handle_row(handle_trees, South, East, False, False)
+	handle_row(handle_trees, South, East, True, False)
 	
-	handle_row(handle_carrot, North, East, False, False)
+	handle_row(handle_carrot, North, East, True, False)
 	
-	handle_row(handle_trees, South, East, False, False)
+	handle_row(handle_trees, South, East, True, False)
+
+	handle_row(handle_carrot, North, East, True, False)
+	
+	handle_row(handle_trees, South, East, True, False)
+	
+	handle_row(handle_carrot, North, East, True, False)
+	
+	handle_row(handle_trees, South, East, True, False)
 	
 
 	#return route
